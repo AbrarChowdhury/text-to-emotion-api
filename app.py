46c16aca944b0ca51e4b5182400e4d621f030bf6
Sakib Mahmud <34135349+sakib00@users.noreply.github.com>
2023-10-18 12:25:36 +0600

    fix

--- app.py
+++ app.py
@@ -30,4 +30,6 @@ def process_text():
         return jsonify(error=str(e)), 400
     
 if __name__ == '__main__':
-    app.run(debug=True)
+    context = ('/etc/letsencrypt/live/virtyousandbox.com/privkey.pem', '/etc/letsencrypt/live/virtyousandbox.com/fullchain.pem')
+    app.run(debug=True, host='0.0.0.0', port=5000, ssl_context=context)
+
